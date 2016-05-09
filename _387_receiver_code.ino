7d4128ea07ab8e6d6546d57c9e67be0ffee153a9
Zach Morgan <morganzpm@gmail.com>
2016-05-08 21:23:49 -0400

    Added files via upload

---
+++ _387_receiver_code.ino
@@ -0,0 +1,99 @@
+#include <SoftwareSerial.h>
+#define E2  10 // Enable Pin for motor 1
+#define I3 8  // Control pin 1 for motor 1
+#define I4 9  // Control pin 2 for motor 2
+SoftwareSerial BTserial(2,3);//Rx, Tx
+int led=7;
+int led2=6;
+boolean DEBUG = true;//brings up values for debugging if true on serial monitor
+  
+const byte maxDataLength = 20;          // maxDataLength is the maximum length allowed for received data.
+char receivedChars[maxDataLength+1] ;
+boolean newData = false;               // newData is used to determine if there is a new command
+ 
+void setup()  
+{
+    pinMode(E2, OUTPUT);
+    pinMode(I3, OUTPUT);
+    pinMode(I4, OUTPUT);
+    pinMode(8, OUTPUT); 
+    pinMode(9, OUTPUT); 
+    digitalWrite(7,LOW);
+    digitalWrite(6,LOW);
+ 
+    if (DEBUG)
+    {
+        // open serial communication for debugging and show the sketch name and the date compiled
+        Serial.begin(9600);
+    }
+ 
+    
+    BTserial.begin(9600); 
+    if (DEBUG)  {   Serial.println(F("Serial started at 9600"));     }
+ 
+    newData = false;
+ 
+}
+ 
+ 
+ 
+void loop()  
+{
+         recvWithStartEndMarkers();                // check to see if we have received any new commands
+         if (newData)  {   processCommand();  }    // if we have a new command do something
+}
+ 
+ 
+void processCommand()
+{
+    analogWrite(E2, 255); // Run in full speed
+    newData = false;
+    if (DEBUG)  {   Serial.print("recieved data = ");  Serial.println(receivedChars);         }
+ 
+      if      (strcmp ("FORWARD",receivedChars) == 0)  { 
+        digitalWrite(7,HIGH); 
+        digitalWrite(I3, HIGH);//these are pins that will reverse the output motor;
+        digitalWrite(I4, LOW);
+        }
+      else if (strcmp ("STILL",receivedChars) == 0) { 
+        digitalWrite(6,LOW); 
+        digitalWrite(7,LOW);    
+        digitalWrite(I3, LOW);
+        digitalWrite(I4, LOW);
+    }
+      else if (strcmp ("REVERSE",receivedChars) == 0) { 
+        digitalWrite(6,HIGH);    
+        digitalWrite(I3, LOW);
+        digitalWrite(I4, HIGH);
+    }
+   
+}
+ 
+void recvWithStartEndMarkers() 
+{
+     static boolean recvInProgress = false;
+     static byte ndx = 0;
+     char startMarker = '<';
+     char endMarker = '>';
+ 
+     if (BTserial.available() > 0) 
+     {
+          char rc = BTserial.read();
+          if (recvInProgress == true) 
+          {
+               if (rc != endMarker) 
+               {
+                    if (ndx < maxDataLength) { receivedChars[ndx] = rc; ndx++;  }
+               }
+               else 
+               {
+                     receivedChars[ndx] = '\0'; // terminate the string
+                     recvInProgress = false;
+                     ndx = 0;
+                     newData = true;
+               }
+          }
+          else if (rc == startMarker) { recvInProgress = true; }
+     }
+ 
+}
